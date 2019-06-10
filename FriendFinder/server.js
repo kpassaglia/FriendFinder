5ed5d89fe0212af286cd1721b11126cc32cac0b4
kpassaglia <kpassaglia@gmail.com>
2019-06-10 14:27:32 -0500

    added dependencies and server bioler plate

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,2 +1,25 @@
+// Dependencies
 const express = require("express");
 const path = require("path")
+
+
+// Sets up the Express App
+var app = express();
+var PORT = 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+// Starts the server to begin listening
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
